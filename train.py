1f28327ccfa7450ab743233a2a28d5b3b203d80d
aleju <kontakt@ajung.name>
2015-08-08 21:45:10 +0200

    Add: started with train loop

--- train.py
+++ train.py
@@ -190,8 +190,90 @@ def load_previous_model():
     
         start_epoch = 0
 
-def train_loop(args.identifier, model, optimizer, epoch_start):
+class History(object):
+    def __init__(self):
+        self.loss_train = OrderedDict()
+        self.loss_val = OrderedDict()
+        self.acc_train = OrderedDict()
+        self.acc_val = OrderedDict()
+
+    def add_values(self, epoch, loss_train=None, loss_val=None, acc_train=None, acc_val=None):
+        if loss_train is not None:
+            self.loss_train[epoch] = loss_train
+        if loss_val is not None:
+            self.loss_val[epoch] = loss_val
+        if acc_train is not None:
+            self.acc_train[epoch] = acc_train
+        if acc_val is not None:
+            self.acc_val[epoch] = acc_val
+
+def train_loop(identifier, model, optimizer, epoch_start):
+    # Loop over each epoch, i.e. executes 20 times if epochs set to 20
+    history = History()
+    la_plotter = LossAccPlotter(save_to_filepath=)
     
+    for epoch in range(epoch_start, EPOCHS):
+        print("Epoch", epoch)
+        
+        loss_train_sum = 0
+        loss_val_sum = 0
+        acc_train_sum = 0
+        acc_val_sum = 0
+        
+        # Training (validation later)
+        progbar = generic_utils.Progbar(n_examples_train)
+        
+        # Iterate over each batch in the training data
+        # and train the net on those examples
+        for X_batch, Y_batch in flow_batches(X_train, y_train, pca, embedder, batch_size=cfg["batch_size"], shuffle=True, train=True):
+            loss, acc = model.train_on_batch(X_batch, Y_batch, accuracy=True)
+            progbar.add(len(X_batch), values=[("train loss", loss), ("train acc", acc)])
+            loss_train_sum += (loss * len(X_batch))
+            acc_train_sum += (acc * len(X_batch))
+        
+        # Validation
+        progbar = generic_utils.Progbar(n_examples_val)
+        
+        # Iterate over each batch in the validation data
+        # and calculate loss and accuracy for each batch
+        for X_batch, Y_batch in flow_batches(X_val, y_val, pca, embedder, batch_size=cfg["batch_size"], shuffle=False, train=False):
+            loss, acc = model.test_on_batch(X_batch, Y_batch, accuracy=True)
+            progbar.add(len(X_batch), values=[("val loss", loss), ("val acc", acc)])
+            loss_val_sum += loss
+            acc_val_sum += acc
+
+        # Calculate the loss and accuracy for this epoch
+        # (averaged over all training data batches)
+        loss_train = loss_train_sum / len(X_train)
+        acc_train = acc_train_sum / len(X_train)
+        loss_val = loss_val_sum / len(X_val)
+        acc_val = acc_val_sum / len(X_val)
+        
+        history.add_values(epoch, loss_train=loss_train, loss_val=loss_val, acc_train=acc_train, acc_val=acc_val)
+        
+        # ---
+        # Update plots with new data from this epoch
+        # ---
+        
+        if epoch > 0:
+            la_plotter.add_values(epoch, loss_train=loss_train, loss_val=loss_val, acc_train=acc_train, acc_val=acc_val)
+        
+        if cfg["save_csv_filepath"] is not None:
+            with open(cfg["save_csv_filepath"], "w") as fp:
+                csvw = csv.writer(fp, delimiter=",")
+                rows = [["epoch", "train_loss", "val_loss", "train_acc", "val_acc"]]
+                #data = data + [[r_e, r_tl, r_vl, r_ta, r_va] for r_e, r_tl, r_vl, r_ta, r_va in zip(range(epoch+1), stats_train_loss, stats_val_loss, stats_train_acc, stats_val_acc)]
+                rows.extend(zip(range(epoch+1), stats_train_loss, stats_val_loss, stats_train_acc, stats_val_acc))
+                csvw.writerows(rows)
+        
+        swae = cfg["save_weights_after_epochs"]
+        if swae and swae > 0 and (epoch+1) % swae == 0:
+            print("Saving model...")
+            #save_model(model, cfg["save_model_dir"], model_name + ".at" + str(epoch), cfg["save_model_extension"], use_gzip=cfg["save_model_gzip"])
+            #save_model_config(model, cfg["save_weights_dir"], model_name + ".at" + str(epoch) + ".config")
+            save_model_weights(model, cfg["save_weights_dir"], model_name + ".at" + str(epoch) + ".weights")
+            save_optimizer_state(optimizer, cfg["save_optimizer_state_dir"], model_name + ".at" + str(epoch) + ".optstate", overwrite=True)
+    # ------------------------------
 
 if __name__ == "__main__":
     main()
